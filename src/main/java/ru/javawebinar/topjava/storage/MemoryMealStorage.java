5510aa815392ba4b1716cbbee45b1c85276bf3f6
Tatiana-Abramova <t.abramova.rzn@gmail.com>
2023-11-28 20:58:49 +0300

    Bugfix

--- src/main/java/ru/javawebinar/topjava/storage/MemoryMealStorage.java
+++ src/main/java/ru/javawebinar/topjava/storage/MemoryMealStorage.java
@@ -4,6 +4,8 @@ import ru.javawebinar.topjava.model.Meal;
 
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.concurrent.ConcurrentHashMap;
@@ -16,13 +18,16 @@ public class MemoryMealStorage implements MealStorage {
     private final ConcurrentMap<Integer, Meal> meals = new ConcurrentHashMap<>();
 
     public MemoryMealStorage() {
-        save(new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500));
-        save(new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000));
-        save(new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500));
-        save(new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100));
-        save(new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000));
-        save(new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500));
-        save(new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410));
+        Arrays.asList(
+                        new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500),
+                        new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500),
+                        new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000),
+                        new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500),
+                        new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100),
+                        new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000),
+                        new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500),
+                        new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410))
+                .forEach(this::save);
     }
 
     @Override
@@ -34,11 +39,8 @@ public class MemoryMealStorage implements MealStorage {
     public Meal save(Meal meal) {
         if (meal.getId() == null) {
             meal.setId(getId());
-        } else if (!meals.containsKey(meal.getId())) {
-            return null;
         }
-        meals.put(meal.getId(), meal);
-        return meal;
+        return meals.replace(meal.getId(), meal);
     }
 
     private int getId() {
